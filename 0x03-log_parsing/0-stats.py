e39837b4487845aebbcc3ce6ebe21f694945e0ef
Chizoba8 <99368906+Chizoba8@users.noreply.github.com>
2023-01-19 16:12:55 +0000

    0 commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,49 @@
 #!/usr/bin/python3
-"""Python script that reads stdin line by line and computes metrics"""
+"""
+Task - Script that reads stdin line by line and computes metrics
+"""
 
 import sys
 
 
-def print_n(t_file_size, status):
-    """Prints total file size and status list"""
-    print("File size: {:d}".format(t_file_size))
-    for key, value in sorted(status.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
+    count = 1
+    file_size = 0
 
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
-t_file_size = 0
-count = 0
-try:
-    for line in sys.stdin:
-        args = line.split()
-
-        if len(args) > 2:
-            status_code = args[-2]
-            file_size = int(args[-1])
-
-            if status_code in status:
-                status[status_code] += 1
-
-            t_file_size += file_size
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
             count += 1
-
-            if count == 10:
-                print_n(t_file_size, status)
-                count = 0
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    print_n(t_file_size, status)
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
+    
